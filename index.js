5eba234bbe537f60de8b10de13cc924889190bd1
G3orgeS <112631563+G3orgeS@users.noreply.github.com>
2023-08-22 14:30:03 +0200

    uppdaterade med html

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 "use strict";
 console.log('hej');
-console.log('testar');
+// console.log('testar')
+function combineStrings(a, b) {
+    return a + b;
+}
+const result = combineStrings('hello ', 'world');
+console.log(result);
+function combineHtmlStrings(a, b) {
+    return a + b;
+}
+const htmlresult = combineHtmlStrings('hello ', 'world');
+const outputElement = document.getElementById('output');
+if (outputElement) {
+    outputElement.textContent = result;
+}
--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
 console.log('hej')
 
-console.log('testar')
+// console.log('testar')
+
+function combineStrings(a: string, b: string): string {
+    return a + b
+}
+const result = combineStrings('hello ', 'world');
+console.log(result)
+
+function combineHtmlStrings(a: string, b: string): string{
+    return a + b 
+}
+
+const htmlresult = combineHtmlStrings('hello ', 'world')
+
+const outputElement = document.getElementById('output')
+
+if(outputElement) {
+    outputElement.textContent = result
+}
+
